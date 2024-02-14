75bef13b36ac8588f189fba5f15d7580a40e84dc
Titouan <titouan.dorier@outlook.fr>
2024-02-14 17:46:31 +0100

    Add windows

--- Source/main.py
+++ Source/main.py
@@ -1,7 +1,33 @@
 import pygame
 
+#####fonction#####
+
+
+def draw():
+    win.fill((0,0,0))
+    pygame.display.update()
+
+
+#####porgramme#####
+    
+
+#region definition variable
+displayY = 576
+displayX = 1000
+
+loop = True
 
 pygame.init()
 
+win = pygame.display.set_mode((displayX, displayY))
+pygame.display.set_caption("UUC-G")
+#endregion
 
-print("Hello Word")
+while loop:
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT: loop = False
+    
+    draw()
+
+
+pygame.quit()
