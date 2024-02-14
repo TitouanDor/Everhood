6bc5ea8d92c83ebd2c02d3e01b7185ec8183e7a3
Titouan <titouan.dorier@outlook.fr>
2024-02-14 18:36:36 +0100

    Update scoreMain.py

--- Source/ScoreBoard/scoreMain.py
+++ Source/ScoreBoard/scoreMain.py
@@ -1,33 +1,9 @@
-import pygame
+import pyQt5
 
 #####fonction#####
 
 
-def draw():
-    win.fill((0,0,0))
-    pygame.display.update()
 
 
 #####porgramme#####
     
-
-#region definition variable
-displayY = 576
-displayX = 1000
-
-loop = True
-
-pygame.init()
-
-win = pygame.display.set_mode((displayX, displayY))
-pygame.display.set_caption("ScoreBoard")
-#endregion
-
-while loop:
-    for event in pygame.event.get(): 
-        if event.type == pygame.QUIT: loop = False
-    
-    draw()
-
-
-pygame.quit()
