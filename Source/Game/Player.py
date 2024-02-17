413117d0aa40a6e8be772b88762c481e6e1fb642
Titouan <titouan.dorier@outlook.fr>
2024-02-17 18:24:11 +0100

    Create Player.py

---
+++ Source/Game/Player.py
@@ -0,0 +1,17 @@
+class Player:
+    def __init__(self,position, pv, name) -> None:
+        self.position = position
+        self.pv = pv
+        self.name = name
+        self.x = 0
+        self.y = 0
+
+        def getName(self): return self.name
+
+        def degat(self, d):
+            self.pv -= d
+            
+
+        
+
+    
